239c9908c8a0146c95356db88b5b5613b7216a0e
OpenTech-Consult <opentech.consult@gmail.com>
2022-06-15 11:32:45 +0000

    Creating Sidebar Component
    created a component to show a nicebar containing the user avatar and some static information.
    
    closes #11

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import styled from 'styled-components';
 import Header from './components/header.component';
+import Sidebar from './components/sidebar.component';
 import './App.css';
 
 function App() {
   return (
     <AppWrapper>
       <Header />
+      <div className="app_body">
+        <Sidebar />
+      </div>
     </AppWrapper>
   );
 }
 
 const AppWrapper = styled.div`
     background-color: #f1f2f5;
+    .app_body {
+      display: flex;
+    }
   `;
 
 export default App;
